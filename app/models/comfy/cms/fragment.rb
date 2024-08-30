6bc58af9c0873bfe7d97b83ae5364bd3cd2f62e7
Richard Sun <rdsun0301@gmail.com>
2024-08-30 12:45:22 -0700

    coder for Data in revisions

--- app/models/comfy/cms/fragment.rb
+++ app/models/comfy/cms/fragment.rb
@@ -6,7 +6,7 @@ class Comfy::Cms::Fragment < ActiveRecord::Base
 
   has_many_attached :attachments
 
-  serialize :content, coder: Psych, type: String
+  serialize :content, coder: Psych
 
   attr_reader :files
 
