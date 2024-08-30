1fa97dd1d54fe5d1e27bf4543f4d3abc59c61af9
Richard Sun <rdsun0301@gmail.com>
2024-08-30 12:18:44 -0700

    coder psych type string

--- app/models/comfy/cms/fragment.rb
+++ app/models/comfy/cms/fragment.rb
@@ -6,7 +6,7 @@ class Comfy::Cms::Fragment < ActiveRecord::Base
 
   has_many_attached :attachments
 
-  serialize :content
+  serialize :content, coder: Psych, type: String
 
   attr_reader :files
 
