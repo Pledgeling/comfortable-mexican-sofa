477d78b54244d6ff59506ac1b17b11bcadb374e2
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2025-12-04 16:11:10 -0500

    Move source CSS and JS files to app/sources
    This is to avoid bundling multiple versions of the final assets in Propshaft

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,7 @@
 const { context, build } = require('esbuild')
 
 const config = {
-  entryPoints: [ 'app/assets/javascripts/comfy/admin/cms/application.js' ],
+  entryPoints: [ 'app/sources/javascripts/comfy/admin/cms/application.js' ],
   bundle: true,
   sourcemap: true,
   outdir: 'app/assets/builds/comfy/admin/cms/',
@@ -9,7 +9,7 @@ const config = {
   // Doesn't necessarily reflect the browser versions we actively support
   // Transpile to an older syntax to avoid excess error reports in HB
   target: [ 'safari13' ],
-  nodePaths: [ 'app/assets/javascripts/' ],
+  nodePaths: [ 'app/sources/javascripts/' ],
 }
 
 if (process.argv.includes('--watch'))
